b22ffc8d21c8c46c6952876fd4ba22f6c3f5f95e
basRet <s.j.h.reterink@students.uu.nl>
2022-03-15 16:44:00 +0100

    -made eventlistener for targetselect en changeselect

--- JavaScript/customizer-3000.js
+++ JavaScript/customizer-3000.js
@@ -1,7 +1,7 @@
-function createHTMLElement(htmlElementLink) {
+function createHTMLElementObject(htmlElementLink) {
     if (htmlElementLink instanceof Element){
         return {
-            htmlElementLink: htmlElementLink,
+            htmlElementLink : htmlElementLink,
             color : getComputedStyle(htmlElementLink).getPropertyValue('background-color'),
             fontSize : getComputedStyle(htmlElementLink).getPropertyValue('font-size'),
         };
@@ -9,39 +9,70 @@ function createHTMLElement(htmlElementLink) {
     return {};
     }
 
-    /*These elements are somewhat volatile. If they do not exist in the current DOM structure, it will be an empty object
+    /*These objects are somewhat volatile. If they do not exist in the current DOM structure, it will be an empty object
     * Also, creates an array for the articles and asides, since there can be multiple*/
-const header = createHTMLElement(document.querySelector("header"));
-const body = createHTMLElement(document.querySelector("body"));
-const main = createHTMLElement(document.querySelector("main"));
-const aside = createHTMLElement(document.querySelector("aside"));
-const customizer = createHTMLElement(document.querySelector(".footer__customizer"))
-
+const header = createHTMLElementObject(document.querySelector("header"));
+const footer = createHTMLElementObject(document.querySelector("footer"));
+const body = createHTMLElementObject(document.querySelector("body"));
+const main = createHTMLElementObject(document.querySelector("main"));
+const aside = createHTMLElementObject(document.querySelector("aside"));
 const articleArray = Array.from(document.querySelectorAll("article"))
-
 const sectionArray = Array.from(document.querySelectorAll("section"))
 
+const customizer = createHTMLElementObject(document.querySelector(".footer__customizer"))
+const targetButton = createHTMLElementObject(document.getElementById("footer__customizer__element-target-selector"))
+const changeButton = createHTMLElementObject(document.getElementById("footer__customizer__change-selector"))
+const changeValueButton =createHTMLElementObject(document.getElementById("footer__customizer__change-value-selector"))
 
+/*these represent the object which is currently selected*/
+let selectedTarget = undefined;
+let selectedChangeString = undefined;
+let selectedChangeValue = undefined;
 
-    /*{
-    add to customizer:
+targetButton.htmlElementLink.addEventListener("input", handleTargetSelect);
 
-    htmlElement : undefined,
-    options : {
-        targetButton : {
-            htmlElement : undefined
-        },
-        changeButton : {
-            htmlElement : undefined
-        },
-        changeValueButton : {
-            htmlElement : undefined
-        }
-    },
-    color : undefined,
-    fontSize : undefined
-}*/
+function handleTargetSelect(){
+   let selectedTargetString = targetButton.htmlElementLink.value;
+   switch (selectedTargetString){
+       case "header" :
+           selectedTarget = header;
+           break;
+       case "footer" :
+           selectedTarget = footer;
+           break;
+       case "body" :
+           selectedTarget = body;
+           break;
+       case "main" :
+           selectedTarget = main;
+           break;
+       case "article" :
+           selectedTarget = articleArray;
+           break;
+       case "section" :
+           selectedTarget = sectionArray;
+   }
+}
 
+let x = 1/0;
+console.log(x)
+
+changeButton.htmlElementLink.addEventListener("click", handleChangeSelect);
+
+function handleChangeSelect(){
+    let selectedChangeString = changeButton.value;
+    switch (selectedChangeString){
+        case "color":
+            break;
+        case "font_size":
+            break;
+    }
+
+    /*remove previous button and all children
+    * add new appropriate button (color selector if user wants to change color,
+    * number input if user want to change font size*/
+    document.addelement
+}
 
 
 /*required functionality:
